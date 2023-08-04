836816431f5f462e4e27b082c7c8a412d519fd16
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2023-08-04 15:51:35 +0100

    1094: Configuring test profiles to use new SAPI-G Automated Testing TPP (#152)
    New Software Id: Y6NjA9TOn3aMm9GaPtLwkp
    New Signing Key Id: qfL4CT5GrVgoyXNQtUjF5TIVOXA
    
    https://github.com/SecureApiGateway/SecureApiGateway/issues/1094

--- src/test/kotlin/com/forgerock/sapi/gateway/ob/uk/framework/configuration/Config.kt
+++ src/test/kotlin/com/forgerock/sapi/gateway/ob/uk/framework/configuration/Config.kt
@@ -1,7 +1,7 @@
 package com.forgerock.sapi.gateway.ob.uk.framework.configuration
 
 // OB directory access token to create an SSA
-val OB_SOFTWARE_ID = System.getenv("obSoftwareId") ?: "ebSqTNqmQXFYz6VtWGXZAa"
+val OB_SOFTWARE_ID = System.getenv("obSoftwareId") ?: "Y6NjA9TOn3aMm9GaPtLwkp"
 val OB_ORGANISATION_ID = System.getenv("obOrganisationId") ?: "0015800001041REAAY"
 
 val SCOPES_TPP = System.getenv("scopesTpp") ?: "ASPSPReadAccess TPPReadAccess AuthoritiesReadAccess"
