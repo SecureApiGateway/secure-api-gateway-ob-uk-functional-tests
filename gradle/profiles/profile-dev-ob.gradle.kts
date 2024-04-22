129c4021db9e40c57bb37c08fef9a3561da5e499
Shaun Harrison <shaun.harrison@forgerock.com>
2024-04-22 08:42:31 +0100

    1389: Update PR workflow for using pr template workflow in CI rrepo

--- gradle/profiles/profile-dev-ob.gradle.kts
+++ gradle/profiles/profile-dev-ob.gradle.kts
@@ -40,7 +40,7 @@ val ssaMatlsUrl by extra("https://matls-dirapi.$obHostSufix/organisation/tpp/{or
  * Functional tests configuration
  */
 // servers
-val environment by extra("dev-ob")
+val environment by extra("shaunharrisonfr-ob")
 val amCookieName by extra("iPlanetDirectoryPro")
 val igServer by extra("https://sapig.$environment.forgerock.financial")
 
